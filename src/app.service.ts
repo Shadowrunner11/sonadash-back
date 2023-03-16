b01533a37d747f0f74ad066e400d0619dd1f0fda
shadowrunner11 <pieroramirez810@gmail.com>
2023-03-16 18:19:18 -0500

    :tada: initial commit

---
+++ src/app.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Axios, { AxiosInstance } from 'axios';
+
+@Injectable()
+export class AppService {
+  private axiosClient: AxiosInstance;
+  constructor(private configService: ConfigService) {
+    const token = this.configService.getOrThrow<string>('TOKEN_SONAR');
+    const baseURL = this.configService.get<string>('SONAR_API_URL');
+    this.axiosClient = Axios.create({
+      baseURL,
+      auth: {
+        username: token,
+        password: '',
+      },
+    });
+  }
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  async postData(): Promise<Record<string, unknown>> {
+    const { data } = await this.axiosClient.get('/components/search_projects');
+    return {
+      data,
+    };
+  }
+}
