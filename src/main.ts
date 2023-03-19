2902a018de2e4d84fb106ba0ecd5afca14cf07ef
shadowrunner11 <pieroramirez810@gmail.com>
2023-03-19 14:53:30 -0500

    :sparkles: added exporting csv and grapqhl filters

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,8 @@ async function bootstrap() {
     .addTag('auth', 'needs auth')
     .build();
 
+  app.enableCors();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
