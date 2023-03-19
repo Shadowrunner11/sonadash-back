bb567a8d8fb3143de2a40e0a81361d79571e2ad3
shadowrunner11 <pieroramirez810@gmail.com>
2023-03-19 18:21:50 -0500

    :bug: added formating of date to csv

--- src/issues/issues.service.ts
+++ src/issues/issues.service.ts
@@ -8,6 +8,12 @@ import { buildFilter, getPaginatedResults } from 'src/tools';
 import { IssuesFilter } from './models/issue.graphql';
 import { createObjectCsvStringifier } from 'csv-writer';
 import { omit } from 'lodash';
+import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 @Injectable()
 export class IssuesService {
@@ -29,10 +35,22 @@ export class IssuesService {
     return getPaginatedResults(this.issueModel, params, { ...filter });
   }
 
+  private helperFormatDate(date?: Date) {
+    return dayjs(date).tz('utc').format('DD/MM/YYYY hh:mm:ss');
+  }
+
   async createReport(filter?: IssuesFilter, fields?: (keyof typeof Issue)[]) {
     this.logger.debug(filter);
     const parsedFilter = filter ? buildFilter({ ...filter }) : {};
     const issues = await this.issueModel.find(parsedFilter).lean();
+    const parsedIssues = issues.map(
+      ({ issueCreatedAt, issueUpdatedAt, ...rest }) => ({
+        ...rest,
+        issueCreatedAt: this.helperFormatDate(issueCreatedAt),
+        issueUpdatedAt: this.helperFormatDate(issueUpdatedAt),
+      }),
+    );
+
     const header = (
       fields ??
       Object.keys(
@@ -42,6 +60,8 @@ export class IssuesService {
 
     const csvWriter = createObjectCsvStringifier({ header });
 
-    return csvWriter.getHeaderString() + csvWriter.stringifyRecords(issues);
+    return (
+      csvWriter.getHeaderString() + csvWriter.stringifyRecords(parsedIssues)
+    );
   }
 }
