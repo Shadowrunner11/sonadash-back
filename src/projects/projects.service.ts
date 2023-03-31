dad1fd2849d58ee71e51b5120b45a361ad1e951f
shadowrunner11 <pieroramirez810@gmail.com>
2023-03-31 10:25:39 -0500

    :sparkles: added columns of date and time

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -2,11 +2,31 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { sonarCollections } from 'src/types';
-import { ProjectsDocument } from './models/projects.schema';
+import { Projects, ProjectsDocument } from './models/projects.schema';
 import { createObjectCsvStringifier } from 'csv-writer';
+import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 @Injectable()
 export class ProjectsService {
+  private readonly headerMetrics = [
+    { id: 'sonarKey', title: 'key' },
+    { id: 'name', title: 'proyecto' },
+    { id: 'observation', title: 'observacion' },
+    { id: 'path', title: 'ruta' },
+    { id: 'qualifier', title: 'tipo' },
+    { id: 'totalCoveragePercent', title: '%Cobertura' },
+    { id: 'linesToCover', title: 'lineas por cubrir' },
+    { id: 'linesNoCoverage', title: 'lineas sin cobertura' },
+    { id: 'linesCoveragePercent', title: '% Cobertura de lineas' },
+    { id: 'dayUpdated', title: 'Dia de muestra' },
+    { id: 'timeUpdated', title: 'Hora de muestra' },
+  ];
+
   constructor(
     @InjectModel(sonarCollections.PROJECTS)
     private projectModel: Model<ProjectsDocument>,
@@ -16,30 +36,32 @@ export class ProjectsService {
     return this.projectModel.findById(id);
   }
 
-  async getReportCoverageMetrics() {
-    const projects = await this.projectModel
-      .find()
-      .select({ sonarKey: 1, name: 1, coverageMetrics: 1, qualifier: 1 })
-      .lean();
-
-    const header = [
-      { id: 'sonarKey', title: 'key' },
-      { id: 'name', title: 'proyecto' },
-      { id: 'observation', title: 'observacion' },
-      { id: 'path', title: 'ruta' },
-      { id: 'qualifier', title: 'tipo' },
-      { id: 'totalCoveragePercent', title: '%Cobertura' },
-      { id: 'linesToCover', title: 'lineas por cubrir' },
-      { id: 'linesNoCoverage', title: 'lineas sin cobertura' },
-      { id: 'linesCoveragePercent', title: '% Cobertura de lineas' },
-    ];
-
-    const parsedProjects = projects.map(({ coverageMetrics, ...rest }) => ({
+  private parseProjects(projects: (Projects & { updatedAt: Date })[]) {
+    return projects.map(({ coverageMetrics, updatedAt, ...rest }) => ({
       ...coverageMetrics,
       ...rest,
+      dayUpdated: dayjs(updatedAt).tz('Etc/GMT-5').format('DD/MM/YYYY'),
+      timeUpdated: dayjs(updatedAt).tz('Etc/GMT-5').format('HH:mm:ss'),
     }));
+  }
 
-    const csvWriter = createObjectCsvStringifier({ header });
+  async getReportCoverageMetrics() {
+    const projects: (Projects & { updatedAt: Date })[] = await this.projectModel
+      .find()
+      .select({
+        sonarKey: 1,
+        name: 1,
+        coverageMetrics: 1,
+        qualifier: 1,
+        updatedAt: 1,
+      })
+      .lean();
+
+    const parsedProjects = this.parseProjects(projects);
+
+    const csvWriter = createObjectCsvStringifier({
+      header: this.headerMetrics,
+    });
 
     return (
       csvWriter.getHeaderString() + csvWriter.stringifyRecords(parsedProjects)
